6dc42b19bbea3a89406f58a8a7cb8174c2bf01c6
mitsuki.okuno <kitsumiokuno@gmail.com>
2021-01-15 14:01:13 +0900

    Add Employee index

--- src/models/validators/EmployeeValidator.java
+++ src/models/validators/EmployeeValidator.java
@@ -9,56 +9,56 @@ import models.Employee;
 import utils.DBUtil;
 
 public class EmployeeValidator {
-    public static List<String> validate(Employee e, Boolean codeDuplicateCheckFlag, Boolean passwordCheckFlag){
+    public static List<String> validate(Employee e, Boolean codeDuplicateCheckFlag, Boolean passwordCheckFlag) {
         List<String> errors = new ArrayList<String>();
 
-        String code_error = validateCode(e.getCode(),codeDuplicateCheckFlag);
-        if(!code_error.equals("")){
+        String code_error = validateCode(e.getCode(), codeDuplicateCheckFlag);
+        if(!code_error.equals("")) {
             errors.add(code_error);
         }
 
         String name_error = validateName(e.getName());
-        if(!name_error.equals("")){
+        if(!name_error.equals("")) {
             errors.add(name_error);
         }
 
-        String password_error = validatePassword(e.getPassword(),passwordCheckFlag);
-        if(!password_error.equals("")){
+        String password_error = validatePassword(e.getPassword(), passwordCheckFlag);
+        if(!password_error.equals("")) {
             errors.add(password_error);
         }
 
         return errors;
     }
 
-    private static String validateCode(String code, Boolean codeDuplicateCheckFlag){
-        if(code == null || code.equals("")){
+    private static String validateCode(String code, Boolean codeDuplicateCheckFlag) {
+        if(code == null || code.equals("")) {
             return "社員番号を入力してください。";
         }
 
-        if(codeDuplicateCheckFlag){
+        if(codeDuplicateCheckFlag) {
             EntityManager em = DBUtil.createEntityManager();
-            long employee_count = (long)em.createNamedQuery("checkRegisterdCode",Long.class)
-                                           .setParameter("code",code)
-                                              .getSingleResult();
+            long employees_count = (long)em.createNamedQuery("checkRegisteredCode", Long.class)
+                                           .setParameter("code", code)
+                                             .getSingleResult();
             em.close();
-            if(employee_count > 0){
-                return "入力された社員番号の情報はすでにそんざいしています。";
+            if(employees_count > 0) {
+                return "入力された社員番号の情報はすでに存在しています。";
             }
         }
 
         return "";
     }
 
-    private static String validateName(String name){
-        if(name == null || name.equals("")){
+    private static String validateName(String name) {
+        if(name == null || name.equals("")) {
             return "氏名を入力してください。";
         }
 
         return "";
     }
 
-    private static String validatePassword(String password, Boolean passwordCheckFlag){
-        if(passwordCheckFlag && (password == null || password.equals(""))){
+    private static String validatePassword(String password, Boolean passwordCheckFlag) {
+        if(passwordCheckFlag && (password == null || password.equals(""))) {
             return "パスワードを入力してください。";
         }
         return "";
